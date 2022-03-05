ee0e1985da392398cad01e156a09d9892e222f4d
singarde <mathieugonza3@gmail.com>
2022-03-05 13:40:42 +0100

    menu principale

--- ruin/www/js/index.js
+++ ruin/www/js/index.js
@@ -25,7 +25,6 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-
+    
     screen.orientation.lock('landscape');
 }
